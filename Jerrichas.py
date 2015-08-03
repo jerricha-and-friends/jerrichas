ed598b7f3a5963c35718fd99b70ae80458c501b4
Jerricha <Jerricha@chat.cohtitan.com>
2015-08-02 22:10:32 -0400

    DB transaction works. Events now

--- Jerrichas.py
+++ Jerrichas.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Requires Python 3
+import os, sys
 VERSION = "0.1.0"
-##### Jerricha's ParagonChat Costume Utility ######
-# Jerrichas.py will automatically replace a costume in your DB with a "costumesave" save.
-# Instructions:
-# 1. Install Python3
-#   1a. (NB: I tried to make this 2-3 compatible, but Python2's
-#     sqlite lib doesn't support WAL)
-# 2. Set PARAGON_CHAT_DB to your ParagonChat.db location. (usually that's %APPDATA%\Paragon Chat\Database\ParagonChat.db). Make sure to CLOSE the ParagonChat program completely.
-#       a. You usually won't have to change this variable,
-#          unless you're on a Mac or have a funky Windows setup!
-# 3. Set COSTUME_FILE to your file that you created with "/costumesave myfile" in Icon.exe. Note this file generated with "costumesave" is NOT the same as the .costume want to set it to the full Windows path, eg, "c:\Program Files\City of Heroes\Data\my_super_costume" but you can also use relative paths.
-# 4. RUN me with 'python ParagonChatCostumeUtil.py' or 'c:\Python34/python.exe ParagonChatCostumeUtil.py'
-## LEGAL: GPLv3. No warrenties. Use it, share it, hack it, but don't sell it!
-####
-####CHANGELOG####
-### Aug-02-2015
-# * Wrote all the things and published
-import os
+__doc__ = """\
+##### Jerricha's ParagonChat Costume Utility v.{} Info ######
+Jerrichas.py will automatically replace a costume in your DB with a "costumesave" save.
+
+Instructions:
+1. Install latest Python 3 from https://www.python.org/downloads/
+2. Set two global variables in this file.
+    a. Set PARAGON_CHAT_DB to your ParagonChat.db location. (usually that's "%APPDATA%\Paragon Chat\Database\ParagonChat.db") (I've already set it there, so you usually don't have to change it.)
+    b. Set COSTUME_FILE to your file that you created with "/costumesave myfile" in Icon.exe. Usually stored in "City of Heroes\Data"
+        *NOTE*: Your costume file is NOT the same as the .costume files that are saved from the Character Creator in Icon.exe. See http://www.cohtitan.com/forum/index.php?topic=11076.0 for more info.
+4. Run me with 'python Jerrichas.py' or 'c:\Python34/python.exe Jerrichas.py'
+5. Please report your errors to me on the forums! This program has really shakey error handling currently!
+
+LEGAL: GPLv3. No warrenties. Use it, share it, hack it, but don't sell it!\
+""".format(VERSION)
 PARAGON_CHAT_DB = os.getenv('appdata') + """\Paragon Chat\Database\ParagonChat.db"""
-# PARAGON_CHAT_DB = """tests\ParagonChat2.db"""
-COSTUME_FILE = """tests\cooltrench_w_boots"""
+COSTUME_FILE = """tests/cooltrench_w_boots"""
+# COSTUME_FILE = """Replace_Me"""
+####################################################
 ###########################
 ### PROGRAM BEINGS HERE ###
 ###########################
@@ -30,7 +30,6 @@ COSTUME_FILE = COSTUME_FILE.replace(os.path.sep, '/')
 
 
 def test_paths():
-    import sys
     try:
         assert os.path.exists(PARAGON_CHAT_DB) is True
     except Exception:
@@ -48,23 +47,35 @@ def test_paths():
 
 def read_costumepart(costume_file=COSTUME_FILE):
     import csv
-    costume_csv = csv.reader(open(costume_file, 'r'))
-    costume = map(
-        lambda row: dict(
-            part=row[0],
-            geom=row[1],
-            tex1=row[2],
-            tex2=row[3],
-            fx=row[39],
-        ),
-        costume_csv
-    )
+    try:
+        costume_csv = csv.reader(open(costume_file, 'r'))
+        costume = map(
+            lambda row: dict(
+                part=row[0],
+                geom=row[1],
+                tex1=row[2],
+                tex2=row[3],
+                fx=row[39] if not row[39]=="0" else "",
+                displayname=row[4],
+                region=row[42],
+                bodyset=row[43],
+                # name=row[],
+                color1=row[5],
+                # color2=row[],
+                # color3=row[],
+                # color4=row[],
+            ),
+            costume_csv
+        )
+    except Exception as e:
+        sys.exit("Some kind of error with your costume file.\n{}".format(e))
     return list(costume)
 
+
 class Database(object):
-    def __init__(self):
+    def __init__(self, db_fp=PARAGON_CHAT_DB):
         import sqlite3
-        self.conn = sqlite3.connect(PARAGON_CHAT_DB)
+        self.conn = sqlite3.connect(db_fp)
         self.conn.row_factory = self._dict_factory
         self.session = self.conn.cursor()
 
@@ -85,46 +96,45 @@ class Database(object):
     def replace_costume(self, character_id, costume_id, costume_file=COSTUME_FILE):
         from io import StringIO
         costumeparts = read_costumepart(costume_file)
-        sql_script = StringIO()
-        # try:
-        #     db.session.execute("BEGIN TRANSACTION")
+        sql_script = StringIO("BEGIN;")
         for i in costumeparts:
             sql =\
-"""\
+                """\
 DELETE FROM costumepart
     WHERE character='{character_id}'
         AND costume='{costume_id}'
         AND part='{part}' ;
-REPLACE INTO costumepart (geom, tex1, tex2, fx, character, costume, part)
-    VALUES ('{geom}', '{tex1}', '{tex2}', '{fx}', '{character_id}', '{costume_id}', '{part}');
-"""
+REPLACE INTO costumepart (geom, tex1, tex2, fx, displayname, region, bodyset, color1, character, costume, part)
+    VALUES ('{geom}', '{tex1}', '{tex2}', '{fx}', '{displayname}', '{region}', '{bodyset}', '{color1}', '{character_id}', '{costume_id}', '{part}');\
+                """
             sql = sql.format(
                 character_id=character_id,
                 costume_id=costume_id,
                 **i
             )
             sql_script.write(sql)
-        print(sql_script.getvalue())
-                # print(sql)
-        #         db.session.executescript(sql)
-        #     db.session.execute("COMMIT TRANSACTION")
-        # except Exception as e:
-        #     print(e)
-        #     db.session.execute("ROLLBACK")
+        try:
+            self.session.executescript(sql_script.getvalue())
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+            self.session.execute("ROLLBACK;")
 
-def event_loop():
-    input()
-    pass
+
+def event_loop(db):
+    while True:
+        account_id = input("\n\nSelect your account id: ")
+        # character_id = input("")
+        # costume_id =
+
 
 def main():
-    print(u"### Jerricha's ParagonChat Costume Utility v{} ###".format(VERSION))
+    print("### <3 Jerricha's ParagonChat Costume Utility v{} <3 ###".format(VERSION))
+    if COSTUME_FILE == "Replace_Me":
+        sys.exit(__doc__)
     test_paths()
-    # event_loop()
-    # if test_paths() == True:
-    #     pass
-    # else:
-    #     print("Error with path: ")
-    # print PARAGON_CHAT_DB
+    db = Database()
+    event_loop(db)
 
 
 if __name__ == '__main__':
