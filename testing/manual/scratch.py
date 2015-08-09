4fcad56de4084c215a199dd4b381a0963bd7873a
Jerricha <Jerricha@chat.cohtitan.com>
2015-08-09 18:33:17 -0400

    scratch w @ROBOKiTTY formulae

--- testing/manual/scratch.py
+++ testing/manual/scratch.py
@@ -1,6 +1,14 @@
-int_convert = lambda x, y, z: c_char(float(z) * 100) << 16 | c_char(float(y) * 100) << 8 | c_char(float(x) * 100)
+# Scartch file for figuring out face scale conversion
+from ctypes import c_char, c_float, c_int, c_byte
 
-# Nose scales
+def int_convert(x, y, z):
+    x, y, z = float(x), float(y), float(z)
+    # return c_char(c_float(z) * 100) << 16 | c_char(c_float(y) * 100) << 8 | c_char(c_float(x) * 100)
+    # return c_char(z * 100) << 16 | c_char(y * 100) << 8 | c_char(c_float(x) * 100)
+    return int(z * 100) << 16 | int(y * 100) << 8 | int(x * 100)
+
+
+# Nose scales from testing/data/analysis/huge.save.csv
 x = "0.110001"
 y = "-0.700002"
 z = "0.089999"
